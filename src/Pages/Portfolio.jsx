43b7447be3dc708da47ce75a2a9714edb2804064
Al-Homaidi <alhomaidi505@gmail.com>
2025-04-08 18:31:33 +0300

    fix package

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useCallback } from "react";
 import data from "../data.json";
 
 import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views";
 import { useTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Tabs from "@mui/material/Tabs";
@@ -277,11 +276,6 @@ export default function FullWidthTabs() {
           </Tabs>
         </AppBar>
 
-        <SwipeableViews
-          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-          index={value}
-          onChangeIndex={setValue}
-        >
           <TabPanel value={value} index={0} dir={theme.direction}>
             <div className="container mx-auto flex justify-center items-center overflow-hidden">
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-5">
@@ -351,7 +345,6 @@ export default function FullWidthTabs() {
               </div>
             </div>
           </TabPanel>
-        </SwipeableViews>
       </Box>
     </div>
   );
